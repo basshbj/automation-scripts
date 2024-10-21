a538fb729ead0377e8ccbf21b9a155fbd6c324e2
basshbj <bassamhbj@gmail.com>
2024-10-21 15:10:03 +0900

    Recreate project set up functionality

--- create_python_project/auto_project_creation.ps1
+++ create_python_project/auto_project_creation.ps1
@@ -11,52 +11,78 @@
 # 8 - Create an empty requirements.txt
 # 9 - Open the project in VSCode
 
+# --- Functions ---
+function Clear-Console {
+  $cursorY  = $Host.UI.RawUI.CursorPosition.Y
+  $cursorX  = $Host.UI.RawUI.CursorPosition.X
+
+  [console]::SetCursorPosition($cursorX, $cursorY)
+}
+
+function Read-Host-Color {
+  param(
+    [string]$message
+  )
+
+  $input = $(Write-Host $message -ForegroundColor Blue -NoNewLine; Read-Host)
+
+  return $input
+}
+
 # --- Script Start ---
 # 1 - Setup Project Directory
-$projectName = Read-Host "Enter the Project Name: "
+Write-Host -ForegroundColor White "Creating the Project folder..."
+$projectName = Read-Host-Color "   Enter the Project Name: "
+$inCurrentDir = Read-Host-Color "   Create in the current directory? (y/n) "
 
-$inCurrentDir = Read-Host "Create in the current directory? (y/n)"
 if ($inCurrentDir -ne "y") {
-  $projectPath = Read-Host "Enter the Project Path: "
+  $projectPath = Read-Host-Color "   | Enter the Project Path: "
   
-  Write-Host "Moving to the Project Path..."
+  Write-Host -ForegroundColor Blue "   | Moving to the Project Path..."
   cd $projectPath
 }
 
-Write-Host "Creating the Project folder..."
+Write-Host -ForegroundColor Blue "   Creating the Project folder..."
 mkdir $projectName
 
 cd $projectName
 
+Write-Host "`r`n`r`nPress any key to continue..."; Read-Host
+
+Clear-Console # Not working
+
 
 # 2 - Initialize Git
-$isInitGit = Read-Host "Initialize Git? (y/n)"
-if ($isInitGit -eq "y") {
-  git init
+Write-Host -ForegroundColor White "Setting up Version Control..."
+$isInitGit = Read-Host-Color "   Initialize Git? (y/n) "
 
-  #gh auth status
 
-  Write-Host "The current Active GitHub Account is: "
-  gh auth status -a
+# if ($isInitGit -eq "y") {
+#   git init
 
-  ## Login to Account
-  $isNewLogin = Read-Host "Login to a different Account? (y/n)"
+#   #gh auth status
 
-  if ($isNewLogin -eq "y") {
-      Write-Host "Logging in to a different Account"
-      gh auth login
-  } else {
-      $isSwitchAccount = Read-Host "Switch Account? (y/n)"
-      if ($isSwitchAccount -eq "y") {
-          Write-Host "Switching Account..."
-          $gitAccount = Read-Host "Enter GitHub Account Name: ..."
-          gh auth switch -u $gitAccount
-      }
-  }
+#   Write-Host "The current Active GitHub Account is: "
+#   gh auth status -a
 
-  gh repo create my_project --public --source=.
+#   ## Login to Account
+#   $isNewLogin = Read-Host "Login to a different Account? (y/n)"
 
-  git add .
-  git commit -m "Initial Commit"
-  git push origin
-}
+#   if ($isNewLogin -eq "y") {
+#       Write-Host "Logging in to a different Account"
+#       gh auth login
+#   } else {
+#       $isSwitchAccount = Read-Host "Switch Account? (y/n)"
+#       if ($isSwitchAccount -eq "y") {
+#           Write-Host "Switching Account..."
+#           $gitAccount = Read-Host "Enter GitHub Account Name: ..."
+#           gh auth switch -u $gitAccount
+#       }
+#   }
+
+#   gh repo create my_project --public --source=.
+
+#   git add .
+#   git commit -m "Initial Commit"
+#   git push origin
+# }
